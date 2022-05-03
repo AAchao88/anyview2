52423be4a48c6d4c220e6cb8fd41ec304fc6e9a5
Wu Jinchao <WJC510175817@126.com>
2022-05-03 23:17:33 +0800

    feat（controller）:写了一个BaseServlet以及完成了用户登录的前端代码

---
+++ controller/src/main/java/com/wjc/servlet/UserServlet.java
@@ -0,0 +1,12 @@
+package com.wjc.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/UserServlet")
+public class UserServlet extends BaseServlet{
+    public void login(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
