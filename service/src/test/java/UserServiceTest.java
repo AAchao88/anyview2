39cb4f3b0453ee6716b235e14e608928003ccde3
Wu Jinchao <WJC510175817@126.com>
2022-05-05 22:38:51 +0800

    feat(service):增加了UserService的测试类与第一个测试方法

---
+++ service/src/test/java/UserServiceTest.java
@@ -0,0 +1,31 @@
+import com.wjc.UserDao;
+import com.wjc.imp.UserDaoImp;
+import com.wjc.pojo.User;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    private UserDao userDao;
+    private User user;
+    @Before
+    public void setUp() throws Exception {
+        userDao = new UserDaoImp();
+        user = new User();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("测试UserServiceImp");
+    }
+
+    @Test
+    public void login() {
+        user.setUserNumber("1234561");
+        user.setPassword("1234561");
+        assertNull(userDao.findByUserNumberAndPassword(user));
+    }
+}
